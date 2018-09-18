ce323d07209289c84a9a411d9bfba72d90634a52
WT <503369325@qq.com>
2018-09-18 13:17:29 +0800

    Add a Flask sample

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import render_template, flash, redirect
+from flask import Flask
+from forms import LoginForm
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+
+# index view function suppressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
+
+
+if __name__ == '__main__':
+    app.run()
